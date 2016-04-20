2f0f532dd7a297f105ca5bd0a74790373aa75bd6
singlepig <gys.nanjing@gmail.com>
2016-04-21 01:06:09 +0800

    delete duplicate bookmarks
    basically implement delete duplicate bookmarks, need to optimise

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 // delete duplicate bookmarks
 
 var allBookmarks = [];
+var allBookmarksTitle = [];
+var duplicateCount = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('button').addEventListener('click', function () {
@@ -21,15 +23,28 @@ function traverse_dir(bookmarkTreeNodes) {
             // if this node is dir then traverse it
             var title = bookmarkTreeNodes[i].title;
             chrome.bookmarks.getChildren(bookmarkTreeNodes[i].id, function (nodes) {
-                console.log("bookmarkTreeNode " + title + " is dir, has " + nodes.length + " items");
+                // console.log("bookmarkTreeNode " + title + " is dir, has " + nodes.length + " items");
                 traverse_dir(nodes);
             });
         } else {
-            allBookmarks.push(bookmarkTreeNodes[i]);
-            console.log(":" + bookmarkTreeNodes[i].title);
+            // console.log("indexOf " + bookmarkTreeNodes[i].title);
+            var index = allBookmarksTitle.indexOf(bookmarkTreeNodes[i].title);
+            // console.log("result: " + index);
+            if (index !== -1) {
+                // already have the node with this title
+                // console.log("count " + duplicateCount++);
+                chrome.bookmarks.remove(bookmarkTreeNodes[i].id);
+                // console.log("--- id: " + bookmarkTreeNodes[i].id);
+                // console.log("--- title: " + bookmarkTreeNodes[i].title);
+                // console.log("--- url: " + bookmarkTreeNodes[i].url);
+                // console.log("=== title: " + allBookmarksTitle[index]);
+            } else {
+                allBookmarks.push(bookmarkTreeNodes[i]);
+                allBookmarksTitle.push(bookmarkTreeNodes[i].title);
+            }
         }
     }
-    console.log("you have " + allBookmarks.length + " bookmarks!");
+    // console.log("you have " + allBookmarks.length + " bookmarks!");
 }
 
 function is_dir(bookmarkTreeNode) {
